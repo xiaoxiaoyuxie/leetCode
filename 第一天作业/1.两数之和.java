8daa5d524075a579b795b05c8c4bd729adf2303d
杨永强 <shixiaoxiaoyuxie@qq.com>
2020-02-07 18:40:21 +0800

    优化算法

--- 第一天作业/1.两数之和.java
+++ 第一天作业/1.两数之和.java
@@ -7,9 +7,11 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int sec = 0;
         for (int i = 0; i < nums.length; i++) {
+            sec = target - nums[i];
             for (int j = i+1; j < nums.length; j++) {
-                if (nums[i]+nums[j] == target){
+                if (nums[j] == sec){
                     return new int[]{i,j};
                 }
             }
